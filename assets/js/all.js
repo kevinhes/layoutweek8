0a1a96e5ce043bdc7a7e0c94e4152d2a1f3d60c9
kevinhes <kevinhes1115@gmail.com>
2021-08-26 15:22:43 +0800

    Update 2021-08-26T07:22:40.007Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,4 +3,43 @@
 $(function () {
   console.log('Hello Bootstrap5');
 });
+var swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  slidesPerView: 'auto',
+  spaceBetween: 30
+});
+var recSwiper = new Swiper('.rec-container', {
+  // Optional parameters
+  slidesPerView: '1',
+  spaceBetween: 30,
+  breakpoints: {
+    992: {
+      slidesPerView: '3' // spaceBetween: 30,
+
+    },
+    768: {
+      slidesPerView: '2' // spaceBetween: 30,
+
+    }
+  }
+});
+var listSwiper = new Swiper('.rec-list', {
+  // Optional parameters
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  slidesPerView: '1',
+  // spaceBetween: 30,
+  breakpoints: {
+    768: {
+      // Optional parameters
+      slidesPerView: '1',
+      // spaceBetween: 0,
+      grid: {
+        rows: 2
+      }
+    }
+  }
+});
 //# sourceMappingURL=all.js.map
