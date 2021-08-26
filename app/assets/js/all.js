7e8373461c08abb420d62b1a2dc446282d7e9781
kevinhes <kevinhes1115@gmail.com>
2021-08-26 15:22:17 +0800

    空間 課程 方案 推薦(需修改)

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,46 @@
 $(function() {
   console.log('Hello Bootstrap5');
 });
+
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  slidesPerView: 'auto',
+  spaceBetween: 30,
+
+});
+
+const recSwiper = new Swiper('.rec-container', {
+  // Optional parameters
+  slidesPerView: '1',
+  spaceBetween: 30,
+  breakpoints: {
+    992: {
+      slidesPerView: '3',
+      // spaceBetween: 30,
+    },
+    768: {
+      slidesPerView: '2',
+      // spaceBetween: 30,
+    }
+  }
+});
+
+const listSwiper = new Swiper('.rec-list', {
+      // Optional parameters
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      slidesPerView: '1',
+      // spaceBetween: 30,
+  breakpoints: {
+    768: {
+      // Optional parameters
+      slidesPerView: '1',
+      // spaceBetween: 0,
+      grid: {
+        rows: 2,
+      },
+    }
+  }
+});
