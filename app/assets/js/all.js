3e8f73d47aec60b88694b084401b936ef248ab0b
kevinhes <kevinhes1115@gmail.com>
2021-08-30 10:27:15 +0800

    預約頁細節

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,9 +1,57 @@
 $(function() {
   console.log('Hello Bootstrap5');
-  $('.lv-select').on("click", function(event) {
+  $('.class-option-first .class-select').on("click", function(event) {
     event.preventDefault();
-    $('.class-lv').slideDown(400);
-    $('.class-choice').toggle();
+    $('.class-lv').slideDown( "slow" );
+    $('.class-choice').slideDown( "slow" );
+    $('.reserve-btn').slideDown( "slow" );
+    $('.class-option-short').removeClass( "selected-short" );
+    $('.class-option-long').removeClass( "selected-long" );
+    $('.class-option-first').addClass( "selected-first" );
+    $('.class-option-short').removeClass( "border-4" );
+    $('.class-option-long').removeClass( "border-4" );
+    $('.class-option-first').addClass( "border-4" );
+  });
+
+  $('.class-option-short .class-select').on("click", function(event) {
+    event.preventDefault();
+    $('.class-lv').slideDown( "slow" );
+    $('.class-choice').slideDown( "slow" );
+    $('.reserve-btn').slideDown( "slow" );
+    $('.class-option-first').removeClass( "selected-first" );
+    $('.class-option-long').removeClass( "selected-long" );
+    $('.class-option-short').addClass( "selected-short" );
+    $('.class-option-first').removeClass( "border-4" );
+    $('.class-option-long').removeClass( "border-4" );
+    $('.class-option-short').addClass( "border-4" );
+  });
+
+  $('.class-option-long .class-select').on("click", function(event) {
+    event.preventDefault();
+    $('.class-lv').slideDown( "slow" );
+    $('.class-choice').slideDown( "slow" );
+    $('.reserve-btn').slideDown( "slow" );
+    $('.class-option-first').removeClass( "selected-first" );
+    $('.class-option-short').removeClass( "selected-short" );
+    $('.class-option-long').addClass( "selected-long" );
+    $('.class-option-first').removeClass( "border-4" );
+    $('.class-option-short').removeClass( "border-4" );
+    $('.class-option-long').addClass( "border-4" );
+  });
+
+  $(document).on( "click", function(event) {
+    var _con = $('.class-select');
+    if (!_con.is(event.target)) {
+      $('.class-lv').slideUp( "slow" );
+      $('.class-choice').slideUp( "slow" );
+      $('.reserve-btn').slideUp( "slow" );
+      $('.class-option-first').removeClass( "selected-first" );
+      $('.class-option-short').removeClass( "selected-short" );
+      $('.class-option-long').removeClass( "selected-long" );
+      $('.class-option-first').removeClass( "border-4" );
+      $('.class-option-short').removeClass( "border-4" );
+      $('.class-option-long').removeClass( "border-4" );
+    }
   });
 });
 
@@ -44,6 +92,31 @@ const listSwiper = new Swiper('.rec-list', {
       // spaceBetween: 0,
       grid: {
         rows: 2,
+      },
+    }
+  }
+});
+
+const classSwiper = new Swiper('.class-list', {
+  // Optional parameters
+  slidesPerView: '1',
+  spaceBetween: 24,
+  grid: {
+    rows: 3,
+  },
+  breakpoints: {
+    992: {
+      slidesPerView: '3',
+      spaceBetween: 30,
+      grid: {
+        rows: 1,
+      },
+    },
+    767: {
+      slidesPerView: '2',
+      spaceBetween: 30,
+      grid: {
+        rows: 1,
       },
     }
   }
